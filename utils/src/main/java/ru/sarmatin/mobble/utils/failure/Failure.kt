a38f8bc934a9ec002cabf367e0038527f26baa77
anton.sarmatin <anton.sarmatin@codezavod.ru>
2020-02-25 16:27:33 +0300

    utils: failure class

---
+++ utils/src/main/java/ru/sarmatin/mobble/utils/failure/Failure.kt
@@ -0,0 +1,22 @@
+package ru.sarmatin.mobble.utils.failure
+
+/**
+ * Created by antonsarmatin
+ * Date: 2020-02-25
+ * Project: Mobble
+ *
+ * Base Class for handling errors/failures/exceptions.
+ * Every feature specific failure should extend [FeatureFailure] class.
+ */
+sealed class Failure {
+
+    object NetworkConnection : Failure()
+
+    object ServerError : Failure()
+
+    /** * Extend this class for feature specific failures.*/
+    abstract class FeatureFailure: Failure()
+
+    /** * This failure may be ignored.*/
+    object Ignore: Failure()
+}
