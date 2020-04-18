cc06dee0cafbf29bea32abc72ae34233fe3ab0d1
antonsarmatin <antonsarmatin@googlemail.com>
2020-04-19 00:54:58 +0300

    fix: javadoc DefaultFullscreen link

--- mv/src/main/java/ru/sarmatin/mobble/mv/platform/MobbleStateFragment.kt
+++ mv/src/main/java/ru/sarmatin/mobble/mv/platform/MobbleStateFragment.kt
@@ -1,5 +1,8 @@
 package ru.sarmatin.mobble.mv.platform
 
+import androidx.lifecycle.Observer
+
+
 /**
  * Created by antonsarmatin
  * Date: 2020-04-16
@@ -8,8 +11,16 @@ package ru.sarmatin.mobble.mv.platform
 
 abstract class MobbleStateFragment : MobbleAbstractFragment() {
 
+    abstract val viewModel: MobbleStateViewModel<*>
+
     //TODO DEFAULT STATE HANDLING
     // Loading, Failure
     // Possibility to implement custom handling?
+
+    protected open val stateObserver: Observer<out MobbleStateViewModel.MobbleState> = Observer {
+        TODO("not implemented")
+    }
+
+
 
 }
