cb65410d8d5e2a01eb2dd34eae660d1d42a5fc20
antonsarmatin <antonsarmatin@googlemail.com>
2020-05-22 19:08:50 +0300

    navigation base architecture

---
+++ nav/src/main/java/ru/sarmatin/mobble/nav/platform/MobbleNavActivity.kt
@@ -0,0 +1,17 @@
+package ru.sarmatin.mobble.nav.platform
+
+import ru.sarmatin.mobble.mv.platform.MobbleActivity
+import ru.sarmatin.mobble.nav.common.NavHost
+import ru.sarmatin.mobble.nav.common.Navigator
+import ru.sarmatin.mobble.nav.router.Router
+import kotlin.reflect.KClass
+
+/**
+ * Created by antonsarmatin
+ * Date: 21/05/2020
+ * Project: Mobble
+ */
+abstract class MobbleNavActivity() : MobbleActivity(), NavHost {
+
+
+}
---
+++ nav/src/main/java/ru/sarmatin/mobble/nav/platform/MobbleNavApplication.kt
@@ -0,0 +1,15 @@
+package ru.sarmatin.mobble.nav.platform
+
+import ru.sarmatin.mobble.mv.platform.MobbleApplication
+
+/**
+ * Created by antonsarmatin
+ * Date: 21/05/2020
+ * Project: Mobble
+ */
+abstract class MobbleNavApplication : MobbleApplication() {
+
+
+
+
+}
